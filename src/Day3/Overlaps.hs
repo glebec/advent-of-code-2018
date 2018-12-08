22027a14ea20317d14cf8466d88d442a1fb828c3
Gabriel Lebec <glebec@gmail.com>
2018-12-07 20:31:45 -0500

    day 3: part 1 vector refactor

--- src/Day3/Overlaps.hs
+++ src/Day3/Overlaps.hs
@@ -1,6 +1,7 @@
 module Day3.Overlaps where
 
 import Text.Trifecta
+import qualified Data.Vector.Unboxed as V
 import Data.Maybe (catMaybes)
 import Day3.Input (raw)
 
@@ -32,27 +33,26 @@ resToMaybe _ = Nothing
 claims :: [Claim]
 claims = catMaybes $ resToMaybe . parseString parseClaim mempty <$> claimsRaw
 
+type UnboxedClaim = (Int, Int, Int, Int)
+
+vClaims :: V.Vector UnboxedClaim
+vClaims = V.fromList $ (\(Claim _ x y w h) -> (x, y, w, h)) <$> claims
+
 -- given a 1 square inch cell whose upper-left corner is (a, b)
-cellInClaim :: Int -> Int -> Claim -> Bool
-cellInClaim a b (Claim _ x y w h) =
+cellInClaim :: Int -> Int -> UnboxedClaim -> Bool
+cellInClaim a b (x, y, w, h) =
     (x <= a && a < x + w) &&
     (y <= b && b < y + h)
 
-cellOverlapped :: Int -> Int -> [Claim] -> Bool
-cellOverlapped a b = go 0 where
-    go n _ | n > 1 = True
-    go n [] = False
-    go n (c:cs) =
-        if cellInClaim a b c
-        then go (n + 1) cs
-        else go n cs
+cellOverlapped :: V.Vector UnboxedClaim -> Int -> Int -> Bool
+cellOverlapped cs a b = 2 `V.elem` V.scanl go (0 :: Int) cs where
+    go n c = if cellInClaim a b c then n + 1 else n
 
-getOverlap :: [Claim] -> Int
-getOverlap cs = length $ filter (== True) overlaps where
-    overlaps = do
-        x <- [1..998]
-        y <- [1..998]
-        pure $ cellOverlapped x y cs
+getOverlap :: V.Vector UnboxedClaim -> Int
+getOverlap cs = V.length $ V.filter (== True) overlaps where
+    overlaps = V.map (uncurry $ cellOverlapped cs) coords
+    coords = V.fromList [(x, y) | x <- dim, y <- dim]
+    dim = [1..999]
 
 -- solution to part 1
-areaOverlapped = getOverlap claims
+areaOverlapped = getOverlap vClaims
